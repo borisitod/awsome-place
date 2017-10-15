000f317d1cf87939a5fd654e0e13e6877850ab02
Boris <boris.chen@kdis.com>
2017-10-16 10:06:14 +1100

    add Google Map component

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgForm} from "@angular/forms";
+import {ModalController} from "ionic-angular";
+import {SetLocationPage} from "../set-location/set-location";
 
 
 @Component({
-  selector: 'page-add-place',
-  templateUrl: 'add-place.html',
+    selector: 'page-add-place',
+    templateUrl: 'add-place.html',
 })
 export class AddPlacePage {
 
-  onSubmit(form: NgForm) {
-    console.log(form.value);
-  }
+    constructor(private modalCtrl: ModalController) {}
+
+    onSubmit(form: NgForm) {
+        console.log(form.value);
+    }
+
+    onOpenMap() {
+        const modal = this.modalCtrl.create(SetLocationPage);
+        modal.present();
+    }
+
 
 }
